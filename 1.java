9a7b732ebb6ce831096f725a2f10018573de7b4f
sindhu kadiri <sindhukadiri444@gmail.com>
2020-03-06 16:56:04 +0530

    my submission

---
+++ 1.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String args[])
+	{
+	    float n=25;
+		double s;
+		s=Math.sqrt(n);
+		if(s-Math.floor(s)==0)
+	{
+	    System.out.println("number is a prefect square");
+	}
+	else
+	{
+	    System.out.println("number is not a prefect square");
+	}
+	}
+}
